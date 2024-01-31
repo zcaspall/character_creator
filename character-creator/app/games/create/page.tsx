bd63a19ff1e24236b5cd2d49910ddf57f420d775
zcaspall <zcaspall@hotmail.com>
2024-01-31 09:56:40 -0600

    game logic

--- character-creator/app/games/create/page.tsx
+++ character-creator/app/games/create/page.tsx
@@ -5,26 +5,30 @@ import { cookies } from "next/headers"
 export default function CreateGame () {
     const createGame = async (formData: FormData) => {
         'use server'
-
+    
         const gameName = formData.get('gameName') as string
-        const inviteCode = crypto.getRandomValues().toString()
+        const inviteCode = Math.random().toString(36).slice(2)
+        const created = new Date()
         const cookieStore = cookies()
         const supabase = createClient(cookieStore)
         const user = (await supabase.auth.getSession()).data.session?.user;
 
         if (!user) {
-            alert("Oops you need to login")
-            return redirect("/login")
+            return redirect("/login?message=Could not authenticate user")
         }
 
-        console.log(inviteCode)
-
-        // const { error } = await supabase
-        //     .from('Games')
-        //     .insert({
-        //         gm_id: user.id,
-        //         invite_code: 
-        //     })
+        const { error } = await supabase
+            .from('Games')
+            .insert({
+                gm_id: user.id,
+                invite_code: inviteCode,
+                name: gameName,
+                created: created
+            })
+        
+        if (error) {
+            throw error
+        }
     }
 
     return (
--- character-creator/components/GameContainer.tsx
+++ character-creator/components/GameContainer.tsx
@@ -10,39 +10,40 @@ export default function GameContainer({ user }: { user: User }) {
     const [runningGameCards, setRunningGameCards] = useState<React.ReactNode[]>([])
     const [playingGameCards, setPlayingGameCards] = useState<React.ReactNode[]>([])
 
-    // const getGames = useCallback(async () => {
-    //     try {
-    //         const { data, error, status } = await supabase
-    //             .from('Games')
-    //             .select(`id, gm_id, invite_code, name`)
-    //             .or(`gm_id.eq.${user.id}, players.cs.${user.id}`)
+    const getGames = useCallback(async () => {
+        try {
+            const { data, error, status } = await supabase
+                .from('Games')
+                .select()
 
-    //         if (error && status !== 406) {
-    //             throw error
-    //         }
+            if (error && status !== 406) {
+                throw error
+            }
 
-    //         if (data) {
-    //             data.forEach(game => {
-    //                 if (game.gm_id === user.id) {
-    //                     setRunningGameCards([
-    //                         <GameCard id={game.id} name={game.name} playerCount={(game.players) ? (game.players.length) : (0)} />
-    //                     ])
-    //                 } else {
-    //                     setPlayingGameCards([
-    //                         <GameCard id={game.id} name={game.name} playerCount={(game.players) ? (game.players.length) : (0)} />
-    //                     ])
-    //                 }
-    //             })
-    //         }
-    //     } catch (error) {
-    //         alert('Error loading game data!')
-    //         console.log(error)
-    //     }
-    // }, [user, supabase])
+            console.log(data)
 
-    // useEffect(() => {
-    //     getGames()
-    // }, [user, supabase])
+            if (data) {
+                data.forEach(game => {
+                    if (game.gm_id === user.id) {
+                        setRunningGameCards([
+                            ...runningGameCards, <GameCard id={game.id} name={game.name} playerCount={(game.players) ? (game.players.length) : (0)} />
+                        ])
+                    } else {
+                        setPlayingGameCards([
+                            <GameCard id={game.id} name={game.name} playerCount={(game.players) ? (game.players.length) : (0)} />
+                        ])
+                    }
+                })
+            }
+        } catch (error) {
+            alert('Error loading game data!')
+            console.log(error)
+        }
+    }, [user, supabase])
+
+    useEffect(() => {
+        getGames()
+    }, [user, getGames])
 
     return (
         <div className=''>
