cfb395236ef1d583a28896d05d3e80e6faff6ae4
zcaspall <zcaspall@hotmail.com>
2024-03-07 19:45:19 -0600

    Creating game view

--- character-creator/app/games/gm/[slug]/page.tsx
+++ character-creator/app/games/gm/[slug]/page.tsx
@@ -1,8 +1,21 @@
-
+'use client';
+import React from 'react';
+import { Tab, Tabs } from '@nextui-org/react';
 export default function gamePageGmView() {
     return (
-        <div>
-        <h1>Game Page GM View</h1>
+        <div className='gm-nav'>
+            <Tabs color='warning'>
+                <Tab key='player-characters' title='Player Characters'>
+                </Tab>
+                <Tab key='npcs' title='NPCs'>
+                </Tab>
+                <Tab key='locations' title='Locations'>
+                </Tab>
+                <Tab key='sessions' title='Sessions'>
+                </Tab>
+                <Tab key='story' title='Story'>
+                </Tab>
+            </Tabs>
         </div>
     )
 }
