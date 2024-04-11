3675b0606b7edef5e5c00e111a8d07fc2b1117f7
xaviercharlemagne <xaviercharlemagne728@gmail.com>
2024-04-10 20:08:34 -0500

    Input changes

--- character-creator/app/newchar/page.tsx
+++ character-creator/app/newchar/page.tsx
@@ -1,8 +1,15 @@
-export default function newchar({}) {
-  const createCharacter = async (formData: FormData) => {
-    'use server'
-    <input> 
-    </input>
-    
+import React from "react"
+
+export default function NewChar() {
+  function newchar(formData : FormData){
+    const charname = formData.get("charater name")
+    return(
+      <form action = {newchar}>
+        Character Name: <input name = "character name"/>
+      </form>
+    )
   }
 }
+
+//make sure the CID matches the character and ability scores properly
+// may have to make scores JSON files to make it easier to add stats to DB
--- character-creator/components/NewCharButton.tsx
+++ character-creator/components/NewCharButton.tsx
@@ -1,3 +1,5 @@
+import React from "react"
+
 export default function NewCharButton({}){
     const buttontext = 'Create a new character'
 
