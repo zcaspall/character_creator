2ed59786e02b15c03bad0f5cb729ea981cf06af3
zcaspall <zcaspall@hotmail.com>
2024-04-28 20:24:34 -0500

    fixed player card

--- character-creator/components/GameMasterView/PlayerCard.tsx
+++ character-creator/components/GameMasterView/PlayerCard.tsx
@@ -6,11 +6,11 @@ export default function PlayerCard({ player }: { player: any}) {
 
     console.log(player)
     return (
-        <div className='flex flex-col w-40 h-60 gap-10 my-10 mx-5 bg-opacity-50 hover:bg-card-background-hover 
+        <div className='flex flex-col w-80 h-80 py-10 gap-10 my-10 mx-5 bg-opacity-50 hover:bg-card-background-hover 
                         bg-card-background rounded-xl text-center justify-center items-center text-card-foreground uppercase'>
             <p>{player.Users.email}</p>
             {player.Characters ? (
-                <div className="flex justify-center">
+                <div className="flex justify-center w-full">
                     <HealthBar currentHealth={player.Characters.hp_curr} maxHealth={player.Characters.hp_max} characterName={player.Characters.character_name} />
                 </div>
             ) : (
